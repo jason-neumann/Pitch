8855cca1cebb8e67a87e46360f12df373b9eb2d0
Jason Neumann <neumaja@gmail.com>
2014-10-05 17:32:50 -0500

    adding card class

---
+++ src/com/neucode/pitch/Card.java
@@ -0,0 +1,19 @@
+package com.neucode.pitch;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Point;
+
+public class Card {
+	public Bitmap image;
+	public Point location;
+
+	public Card(Bitmap image, Point location) {
+		this.image = image;
+		this.location = location;
+	}
+	
+	public void draw(Canvas canvas) {
+		canvas.drawBitmap(image, location.x - (image.getWidth() / 2), location.y - (image.getHeight() / 2), null);
+	}
+}
